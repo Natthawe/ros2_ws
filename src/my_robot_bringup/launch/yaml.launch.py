20c6f8ea610de63c2770449670eec155d83f5459
natthawe <natthawejumjai@gmail.com>
2023-02-20 11:36:45 +0700

    add - params.yaml -yaml.launch.py

--- src/my_robot_bringup/launch/yaml.launch.py
+++ src/my_robot_bringup/launch/yaml.launch.py
@@ -1,12 +1,23 @@
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     ld = LaunchDescription()
+    
+    config = os.path.join(
+        get_package_share_directory('my_robot_bringup'),
+        'config',
+        'params.yaml'
+    )
 
     yaml_node = Node(
         package="my_python_pkg",
         executable="params",
+        name="Params_Node",
+        namespace="namespace",
+        parameters= [config]
     )
 
     ld.add_action(yaml_node)
